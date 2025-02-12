9eff6b75ca83dc05f8b3bc21f4e0a91c4713ff89
Adnankharadi <adnankharadi8@gmail.com>
2025-02-12 20:23:29 +0500

    New Filesclear

---
+++ apple.js
@@ -0,0 +1,43 @@
+// var a = 5;
+
+// var b = ++a - a-- - ++2 + a++ + a++ - --a;
+
+// console.log(b);
+
+// var a = 5;
+
+// var b = 3;
+ 
+// var c = ++b - ++a - b++ - a++ - --b + a;
+
+// console.log(c)
+
+// var a = 5;
+
+// var b = 3;
+ 
+// var c = a + --a + b-- + b + --a + ++a -b;
+
+// console.log(c);
+
+
+// var firstname = "Adnan"
+
+// var lastname = "kharadi"
+
+// var age = 20
+
+// console.log("Hello My Name Is",firstname,lastname "and I Am" , age "years old");
+
+
+// chap 9
+
+
+// prompt                //JS Builtin Funtion
+
+
+prompt("Enter Name" , "adnan@gmail.com");   
+
+prompt("Enter Email");    
+
+
